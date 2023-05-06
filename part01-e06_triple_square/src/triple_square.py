c4a29f951a8bb289ffef1bf7cd4094b4f6b48fd6
unknown <tatu.miinalainen@gmail.com>
2023-05-06 22:36:09 +0300

    tehtävät

--- part01-e06_triple_square/src/triple_square.py
+++ part01-e06_triple_square/src/triple_square.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
+def triple(num):
+    return num * 3
+
+def square(num):
+    return num ** 2 
 
 def main():
+    for number in range(1,11):
+        triple_result = triple(number)
+        square_result = square(number)
+        if square_result > triple_result:
+            break
+        else:
+            print(f"triple({number})=={triple_result} square({number})=={square_result}")
     pass
 
 if __name__ == "__main__":
--- part01-e07_areas_of_shapes/src/areas_of_shapes.py
+++ part01-e07_areas_of_shapes/src/areas_of_shapes.py
@@ -2,9 +2,33 @@
 
 import math
 
+def triangle(base, height):
+    return int(base) * int(height) / 2
+
+def rectangle(width, height):
+    return int(width) * int(height)
+
+def circle(radius):
+    return 3.14159265359 * float(radius) * float(radius)
 
 def main():
-    # enter you solution here
+    while True:
+        shape = input("Choose a shape (triangle, rectangle, circle): ").lower()
+        if shape == "triangle":
+            base = input("Give base of the triangle: ")
+            height = input("Give height of the triangle: ")
+            print(f"The area is {triangle(base, height):6f}")
+        elif shape == "rectangle":
+            width = input("Give base of the rectangle: ")
+            height = input("Give height of the rectangle: ")
+            print(f"The area is {rectangle(width, height):6f}")
+        elif shape == "circle":
+            radius = input("Give radius of the circle: ")
+            print(f"The area is {circle(radius):6f}")
+        elif len(shape) == 0:
+            break
+        else:
+            print("Unknown shape!")
 
 if __name__ == "__main__":
     main()
