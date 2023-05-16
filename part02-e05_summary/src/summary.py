d2dfd71700aed7ae498ce9106136ca8846af7754
unknown <tatu.miinalainen@gmail.com>
2023-05-16 19:12:47 +0300

    tehtävät

--- part02-e05_summary/src/summary.py
+++ part02-e05_summary/src/summary.py
@@ -1,22 +1,53 @@
 #!/usr/bin/env python3
 
 import sys
+import statistics
 
-def tes(file):
-    f = open(file, "r")
+def stddev (files):
+    std = 0
+    sample = []
+    f = open(files, "r")
+    for num in f.readlines():
+        try:
+            sample.append(float(num.strip()))
+        except:
+            continue
+    std = statistics.stdev(sample)
+    f.close()
+    return std
+
+def average(files):
     sum = 0
-    num = []
-    for i, line in enumerate(f):
-        num = int(line)
-        print(num)
+    avg = 0
+    f = open(files, "r")
+    for lenght, num in enumerate(f):
+        try:
+            sum += float(num.strip())
+        except:
+            continue
+    avg = sum/(lenght+1)
+    f.close()
+    return avg
+
+def sum(files):
+    sum = 0
+    f = open(files, "r")
+    for num in f.readlines():
+        try:
+            sum += float(num.strip())
+        except:
+            continue
+    f.close()
     return sum
     
 def summary(filename):
-
-    return ()
+    return (sum(filename), average(filename), stddev(filename))
 
 def main():
-    print(summary())
+    files = sys.argv[1:]
+    for file in files:
+        result = summary(file)
+        print(f"File: {file} Sum: {result[0]:.6f} Average: {result[1]:.6f} Stddev: {result[2]:.6f}")
     pass
 
 if __name__ == "__main__":
--- part02-e06_file_count/src/file_count.py
+++ part02-e06_file_count/src/file_count.py
@@ -3,9 +3,23 @@
 import sys
 
 def file_count(filename):
-    return (0, 0, 0)
+    lines = 0
+    words = 0
+    characters = 0
+    f = open(filename, "r")
+    for line in f.readlines():
+        lines += 1
+        splitted_line = line.split()
+        words += len(splitted_line)
+        characters += sum(len(word) for word in line)
+
+    return (lines, words, characters)
 
 def main():
+    files = sys.argv[1:]
+    for file in files:
+        result = file_count(file)
+        print(result)
     pass
 
 if __name__ == "__main__":
